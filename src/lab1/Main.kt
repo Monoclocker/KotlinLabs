c09f9100c53def58f0cce6df01c6df506882da28
Саша Евкин <evkin.a.2002@gmail.com>
2024-10-06 22:02:34 +0300

    1 лабораторная

--- src/lab1/Main.kt
+++ src/lab1/Main.kt
@@ -54,8 +54,8 @@ fun main(){
 
     val secondFigureCalculations = calculateSquareAndPerimeter(*secondFigure)
 
-    println("Площадь первой фигуры: ${secondFigureCalculations.first} \n" +
-            "Периметр первой фигуры: ${secondFigureCalculations.second}")
+    println("Площадь второй фигуры: ${secondFigureCalculations.first} \n" +
+            "Периметр второй фигуры: ${secondFigureCalculations.second}")
 
     val intersectionResult = isIntersect(firstFigure, secondFigure)
 
@@ -69,12 +69,30 @@ fun main(){
 
 fun coordinatesInput(): Pair<Double, Double> {
 
-    println("Введите координату X")
-    val x: Double = readln().toDouble()
+    val x: Double
+    val y: Double
 
-    println("Введите координату Y")
-    val y: Double = readln().toDouble()
+    while (true){
+        println("Введите координату X")
+        val input = readln().toDoubleOrNull()
+        if (input == null){
+            println("Ввод не является числом")
+            continue
+        }
+        x = input
+        break
+    }
 
+    while (true){
+        println("Введите координату Y")
+        val input = readln().toDoubleOrNull()
+        if (input == null){
+            println("Ввод не является числом")
+            continue
+        }
+        y = input
+        break
+    }
     return Pair(x, y)
 }
 
@@ -109,17 +127,126 @@ fun calculatePerimeterWithCondition(firstPoint: Pair<Double, Double>,
         return abs(secondPoint.first - firstPoint.first)
     }
     else{
-        val firstCatheter: Double = abs(firstPoint.first - firstPoint.first)
-        val secondCatheter: Double = abs(secondPoint.second - secondPoint.second)
+        val firstCatheter: Double = abs(firstPoint.first - secondPoint.first)
+        val secondCatheter: Double = abs(firstPoint.second - secondPoint.second)
         return sqrt(firstCatheter.pow(2) + secondCatheter.pow(2))
     }
 }
 
+fun areIntersecting(p1: Pair<Double,Double>, p2: Pair<Double,Double>, q1: Pair<Double,Double>, q2: Pair<Double,Double>)
+    : Pair<Double,Double>? {
+    val a1 = p2.second - p1.second
+    val b1 = p1.first - p2.first
+    val c1 = a1 * p1.first + b1 * p1.second
 
+    val a2 = q2.second - q1.second
+    val b2 = q1.first - q2.first
+    val c2 = a2 * q1.first + b2 * q1.second
+
+    val det = a1 * b2 - a2 * b1
+
+    if (det == 0.0) {
+        return null
+    } else {
+        val x = (b2 * c1 - b1 * c2) / det
+        val y = (a1 * c2 - a2 * c1) / det
+
+        val onSegment1 = minOf(p1.first, p2.first) <= x && x <= maxOf(p1.first, p2.first) &&
+                minOf(p1.second, p2.second) <= y && y <= maxOf(p1.second, p2.second)
+        val onSegment2 = minOf(q1.first, q2.first) <= x && x <= maxOf(q1.first, q2.first) &&
+                minOf(q1.second, q2.second) <= y && y <= maxOf(q1.second, q2.second)
+
+        return if (onSegment1 && onSegment2) {
+            Pair(x, y)
+        } else {
+            null // Точка пересечения не находится на обоих отрезках
+        }
+    }
+}
+
+fun isPointInsidePolygon(point: Pair<Double, Double>, polygon: Array<Pair<Double, Double>>): Boolean {
+    var intersections = 0
+    val size = polygon.size
+    for (i in polygon.indices) {
+        val p1 = polygon[i]
+        val p2 = polygon[(i + 1) % size]
+
+        if ((point.second > minOf(p1.second, p2.second)) && (point.second <= maxOf(p1.second, p2.second)) &&
+            (point.first <= maxOf(p1.first, p2.first))) {
+            val xIntersections = (point.second - p1.second) * (p2.first - p1.first) / (p2.second - p1.second) + p1.first
+            if (p1.first == p2.first || point.first <= xIntersections) {
+                intersections++
+            }
+        }
+    }
+    return intersections % 2 != 0
+}
 
 fun isIntersect(first: Array<Pair<Double,Double>>, second: Array<Pair<Double, Double>>) : Pair<Boolean, Double?>{
 
-    //TODO: Придумать реализацию
+    val firstClone = first.clone()
+    val secondClone = second.clone()
 
-    return Pair(false, null)
+    var shouldReverse = false
+
+    for (pointNum in first.indices){
+        if (first[pointNum].first > first[pointNum + 1].first)
+            shouldReverse = true
+        break
+    }
+
+    if(shouldReverse){
+        firstClone.reverse()
+        shouldReverse = false
+    }
+
+    for (pointNum in second.indices){
+        if (second[pointNum].first > second[pointNum + 1].first)
+            shouldReverse = true
+        break
+    }
+
+    if (shouldReverse){
+        secondClone.reverse()
+    }
+
+    val haveOutsideArea: Boolean = secondClone.any { !isPointInsidePolygon(it, firstClone) }
+
+    if (!haveOutsideArea){
+        return Pair(true, calculateSquareAndPerimeter(*secondClone).first)
+    }
+
+    var intersectionMeansExit: Boolean = isPointInsidePolygon(secondClone.first(), firstClone)
+
+    var intersectionPoints: Array<Pair<Double, Double>> = emptyArray()
+
+    val firstSize = firstClone.size - 1
+    val secondSize = secondClone.size - 1
+
+    for(secondPointNum in secondClone.indices){
+
+        for (firstPointNum in firstClone.indices){
+
+            if (intersectionMeansExit){
+                intersectionPoints += Pair(secondClone[secondPointNum].first, secondClone[secondPointNum].second)
+            }
+
+            val intersectionPoint = areIntersecting(secondClone[secondPointNum % secondSize],
+                secondClone[(secondPointNum + 1) %  secondSize], firstClone[firstPointNum % firstSize],
+                firstClone[(firstPointNum + 1) % firstSize])
+
+            if (intersectionPoint == null){
+                continue
+            }
+
+            intersectionPoints += Pair(intersectionPoint.first, intersectionPoint.second)
+            intersectionMeansExit = !intersectionMeansExit
+        }
+    }
+
+    if (intersectionPoints.contentEquals(secondClone)){
+        return Pair(false, null)
+    }
+
+    return Pair(true, calculateSquareAndPerimeter(*intersectionPoints).first)
 }
